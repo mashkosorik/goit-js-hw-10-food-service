fe4b1a6fb886ec4d38e927c2e88966599a0c3dc8
mashkosorik <mahandsorik@gmail.com>
2021-08-27 13:09:51 +0300

    f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import './sass/main.scss';
+import css from "./css/style.css";
+import post from "./post.hbs";
+import menu from "./menu.json";
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+const themeSwitchToggle = document.querySelector("#theme-switch-toggle");
+const body = document.querySelector("body");
+const jsMenu = document.querySelector(".js-menu");
+const menuItem = post(menu);
+jsMenu.insertAdjacentHTML("afterbegin", menuItem);
+
+if (localStorage.getItem('theme')) {
+  body.classList.add(Theme.DARK);
+  themeSwitchToggle.checked = true
+}
+
+
+themeSwitchToggle.addEventListener ("change", (event) =>{
+if (event.target.checked) {
+  body.classList.add(Theme.DARK);
+  body.classList.remove(Theme.LIGHT);
+  localStorage.setItem('theme', '!')
+} else {
+  body.classList.remove(Theme.DARK);
+  body.classList.add(Theme.LIGHT);
+  localStorage.removeItem('theme')
+}
+});
+
